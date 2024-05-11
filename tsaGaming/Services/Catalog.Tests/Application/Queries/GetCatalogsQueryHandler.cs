4f870d87ca10467dda293e52dd9390a6d34d1894
AcMaTeo <anhthong3010@gmail.com>
2024-05-11 20:11:47 +0700

    add NUnit and Jenkinfile template

---
+++ tsaGaming/Services/Catalog.Tests/Application/Queries/GetCatalogsQueryHandler.cs
@@ -0,0 +1,66 @@
+﻿using Catalog.API.Application.Queries;
+using Catalog.Domain.Entites;
+using Catalog.Domain.Interfaces;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Tests.Application.Queries
+{
+    [TestFixture]
+    public class GetCatalogsQueryHandler_GetAll
+    {
+        private IList<Catalog.Domain.Entites.Catalog> result;
+        private GetCatalogsQueryHandler _getCatalogsQueryHandler;
+
+        [SetUp]
+        public void Setup()
+        {
+            result = new List<Catalog.Domain.Entites.Catalog>
+            {
+                new Catalog.Domain.Entites.Catalog
+                {
+                    DisplayName = "DisplayName",
+                    ImageUrl = "ImageUrl",
+                    Name = "Catalog",
+                    Lessons = new List<Lesson>()
+                    {
+                        new Lesson
+                        {
+                            Name = "Lesson",
+                            Games = new List<Game>()
+                            {
+                                new Game()
+                                {
+                                    Name = "Game"
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var logger = Mock.Of<ILogger<GetCatalogsQueryHandler>>();
+
+            var _catalogRepository = new Mock<ICatalogRepository>();
+            _catalogRepository.Setup(x => x.GetAllAsync(It.IsAny<int>(), It.IsAny<int>()))
+                    .ReturnsAsync(result)
+                    .Verifiable("Notification was not sent.");
+
+            _getCatalogsQueryHandler = new GetCatalogsQueryHandler(_catalogRepository.Object, logger);
+        }
+
+        [Test]
+        public void GetCatalogsQueryHandler_GetAll_Return_Value()
+        {
+            var result = _getCatalogsQueryHandler.Handle(new GetCatalogsQuery(), default(CancellationToken));
+
+            Assert.IsNotNull(result.Result);
+        }
+    }
+}
---
+++ tsaGaming/Services/Catalog.Tests/Controller/CatalogController.cs
@@ -0,0 +1,66 @@
+﻿using Catalog.API.Application.Queries;
+using Catalog.API.Controllers;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Ocelot.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Tests.Controller
+{
+    [TestFixture]
+    public class CatalogController_GetAll
+    {
+        private CatalogController _catalogController;
+        private IList<CatalogDTO> result;
+
+        [SetUp]
+        public void SetUp()
+        {
+            result = new List<CatalogDTO>
+            {
+                new CatalogDTO
+                {
+                    DisplayName = "DisplayName",
+                    ImageUrl = "ImageUrl",
+                    Name = "Catalog",
+                    Lessons = new List<LessonDTO>()
+                    {
+                        new LessonDTO
+                        {
+                            Name = "Lesson",
+                            Games = new List<GameDTO>()
+                            {
+                                new GameDTO()
+                                {
+                                    Name = "Game"
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var logger = Mock.Of<ILogger<CatalogController>>();
+
+            var mediator = new Mock<IMediator>();
+            mediator.Setup(x => x.Send(It.IsAny<GetCatalogsQuery>(), default(CancellationToken)))
+                    .ReturnsAsync(result)
+                    .Verifiable("Notification was not sent.");
+
+            _catalogController = new CatalogController(logger, mediator.Object);
+        }
+
+        [Test]
+        public void CatalogController_GetAll_Return_Value()
+        {
+            var result = _catalogController.Get(new GetCatalogsQuery());
+
+            Assert.IsNotNull(result.Result);
+        }
+    }
+}
